517b7a7934fddffad5b23130b0cd0628300538b4
jinsu <biosvos@gmail.com>
2026-01-30 17:36:58 +0900

    feat: progress bar 활성화

--- src/vectorstore/embeddings.py
+++ src/vectorstore/embeddings.py
@@ -1,28 +1,5 @@
 import logging
-import os
-import sys
 import warnings
-
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
-os.environ["TQDM_DISABLE"] = "1"
-
-from unittest.mock import patch
-
-import tqdm
-
-_original_tqdm = tqdm.tqdm
-
-
-class SilentTqdm(_original_tqdm):
-    def __init__(self, *args, **kwargs):
-        kwargs["disable"] = True
-        super().__init__(*args, **kwargs)
-
-
-tqdm.tqdm = SilentTqdm
-sys.modules["tqdm"].tqdm = SilentTqdm
-sys.modules["tqdm.auto"].tqdm = SilentTqdm
 
 import transformers
 from sentence_transformers import SentenceTransformer
